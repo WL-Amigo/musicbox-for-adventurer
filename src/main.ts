6ecaeef0404f87ad212430a0c4f4727be9f37936
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2021-04-04 15:49:16 +0900

    refs #17 fix HMR broke by polyfill

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,10 @@ import App from './App.vue';
 import 'virtual:windi.css';
 
 createApp(App).mount('#app');
+
+if (process.env.NODE_ENV === 'development' && window.global) {
+  // polyfill の影響で HMR ランタイムが window.global (ブラウザにおけるグローバルではない)に
+  // 飛んでしまうため、本来のグローバル環境に移し替える
+  // @ts-ignore
+  window.__VUE_HMR_RUNTIME__ = window.global.__VUE_HMR_RUNTIME__;
+}
